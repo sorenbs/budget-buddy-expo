325f4e84d384e71384bb2a038318daae9e8dcb9a
Søren Bramer Schmidt <sorenbs@gmail.com>
2024-03-19 17:00:57 +0700

    Updated to latest version of Prisma

--- db.ts
+++ db.ts
@@ -1,14 +1,6 @@
 import '@op-engineering/react-native-prisma';
 import { PrismaClient } from '@prisma/client/rn';
-
-import 'react-native-url-polyfill/auto';
-import { atob, btoa } from 'react-native-quick-base64';
 import { reactiveHooksExtension } from "@op-engineering/react-native-prisma";
-
-global.atob = atob;
-global.btoa = btoa;
-
-console.log("creating Prisma!!!!! Should only happen once !!!!!")
 
 const basePrisma = new PrismaClient()
 
